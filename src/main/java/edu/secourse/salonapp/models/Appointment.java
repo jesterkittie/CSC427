8ea4615f6cd104e9ee45bd34502e3ee6fa722874
jesterkittie <131824820+jesterkittie@users.noreply.github.com>
2026-04-23 16:30:20 -0400

    User class complete

--- src/main/java/edu/secourse/salonapp/models/Appointment.java
+++ src/main/java/edu/secourse/salonapp/models/Appointment.java
@@ -1,4 +1,5 @@
 package edu.secourse.salonapp.models;
 
 public class Appointment {
+    //there will be variables appointmentId,customerId,stylistId,date,time
 }
--- src/main/java/edu/secourse/salonapp/models/User.java
+++ src/main/java/edu/secourse/salonapp/models/User.java
@@ -1,4 +1,69 @@
 package edu.secourse.salonapp.models;
 
 public class User {
+    //account number assignment
+    private static int nextAccountNumber = 1;
+
+    private int accountNumber;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String role; // customer, admin, stylist
+
+    // constructor
+    public User(String user, String pass, String n, String mail, String r) {
+        accountNumber = nextAccountNumber++;
+        username = username;
+        password = password;
+        name = name;
+        email = email;
+        role = role;
+    }
+
+    // getters
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // setters
+    public void setUsername(String user) {
+        username = user;
+    }
+
+    public void setPassword(String pass) {
+        password = pass;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setEmail(String mail) {
+        email = mail;
+    }
+
+    public void setRole(String r) {
+        role = r;
+    }
 }
